ee394d0b5235547b43cd62405912116a034bf4c8
Иван Соснин <zorrobinnaro@mail.ru>
2022-10-12 23:06:42 +0700

    Доделал описания

--- lab2_simulator/Realty.cpp
+++ lab2_simulator/Realty.cpp
@@ -1,5 +1,6 @@
 #include "Realty.h"
 #include <iostream>
+#include <conio.h>
 
 Realty::Realty()
 {
@@ -26,4 +27,80 @@ void Realty::RealtyDisplay()
 
 void Realty::BuyNew(Human human)
 {
+    int choice, flag;
+    char strbuynew[30];
+    std::cout << "\nВыберите для покупки:\n1 - Квартира(3 000 000р)\n2 - Коттедж(7 000 000р)\n3 - Вилла(20 000 000р)\n4 - Дворец(100 000 000р)\n5 - Мотоцикл(1 000 000)\n6 - Автомобиль(2 000 000)\n";
+    do {
+        flag = 0;
+        choice = _getch();
+        if (choice != '1') flag++;
+        if (choice != '2') flag++;
+        if (choice != '3') flag++;
+        if (choice != '4') flag++;
+    } while (flag == 4);
+    flag = 0;
+    switch (choice) {
+    case '1': {
+        if (human.Moneybalance > 3000000) {
+            std::cout << "Вы купили квартиру за 3 000 000р!";
+            human.Moneybalance -= 3000000;
+            strcpy(strbuynew, "Квартира");
+            Realty(strbuynew, Vehicle);
+        }
+        else flag = 1;
+        break;
+    }
+    case '2': {
+        if (human.Moneybalance > 7000000) {
+            std::cout << "Вы купили коттедж за 7 000 000р!";
+            human.Moneybalance -= 7000000;
+            strcpy(strbuynew, "Коттедж");
+            Realty(strbuynew, Vehicle);
+        }
+        else flag = 1;
+        break;
+    }
+    case '3': {
+        if (human.Moneybalance > 20000000) {
+            std::cout << "Вы купили виллу за 20 000 000р!";
+            human.Moneybalance -= 20000000;
+            strcpy(strbuynew, "Вилла");
+            Realty(strbuynew, Vehicle);
+        }
+        else flag = 1;
+        break;
+    }
+    case '4': {
+        if (human.Moneybalance > 100000000) {
+            std::cout << "Вы купили Дворец за 100 000 000р!";
+            human.Moneybalance -= 100000000;
+            strcpy(strbuynew, "Дворец");
+            Realty(strbuynew, Vehicle);
+        }
+        else flag = 1;
+        break;
+    }
+    case '5': {
+        if (human.Moneybalance > 1000000) {
+            std::cout << "Вы купили мотоцикл за 1 000 000р!";
+            human.Moneybalance -= 1000000;
+            strcpy(strbuynew, "Мотоцикл");
+            Realty(Housing, strbuynew);
+        }
+        else flag = 1;
+        break;
+    }
+    case '6': {
+        if (human.Moneybalance > 2000000) {
+            std::cout << "Вы купили автомобиль за 2 000 000р!";
+            human.Moneybalance -= 2000000;
+            strcpy(strbuynew, "Автомобиль");
+            Realty(Housing, strbuynew);
+        }
+        else flag = 1;
+        break;
+    }
+    }
+    if (flag)
+        std::cout << "\nНедостаточно денег для покупки\n";
 }
